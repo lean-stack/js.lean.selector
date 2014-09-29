9d051892d60d531d93fca6d32a4ea6ca6c83a0fc
Michael Alt <lean@michael-alt.info>
2014-09-29 20:16:23 +0200

    First build: revealing module pattern

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var bump = require('gulp-bump');
+var concat = require('gulp-concat');
+var git = require('gulp-git');
+var header = require('gulp-header');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglifyjs');
+
+var pkg = require('./package.json');
+var banner = '/*! <%= pkg.name %> v<%= pkg.version %> ' +
+             '| (c) 2014 Michael Alt ' +
+             'http://lean-stack.github.io/license.txt */\n';
+
+gulp.task('js-style', function() {
+  return gulp.src(['./gulpfile.js', './src/**/*.js', './test/**/*.js'])
+    .pipe(jscs());
+});
+
+gulp.task('js-lint', function() {
+  return gulp.src(['./gulpfile.js', './src/**/*.js', './test/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(uglify(pkg.name + '.min.js', {
+      outSourceMap: true,
+      basePath: ''
+    }))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['js-lint', 'js-style']);
+
+gulp.task('dist', ['compress'], function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(concat(pkg.name + '.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function(done) {
+  var karma = require('karma').server;
+  karma.start({
+    configFile: __dirname + '/test/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('bump:patch', function() {
+  return gulp.src(['./package.json', './bower.json'])
+    .pipe(bump({type: 'patch'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:feature', function() {
+  return gulp.src(['./package.json', './bower.json'])
+    .pipe(bump({type: 'feature'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('release', function() {
+  var pkg = require('./package.json');
+  var v = 'v' + pkg.version;
+  var message = 'Release ' + v;
+
+  return gulp.src('./')
+    .pipe(git.commit(message))
+    .pipe(git.tag(pkg.version, v))
+    .pipe(git.push('origin', 'master', '--tags'))
+    .pipe(gulp.dest('./'));
+});
---
+++ src/lean.selector.js
@@ -0,0 +1,44 @@
+/*
+ * Lean CSS Selector Engine
+ *
+ * Copyright 2014 Michael Alt <lean@michael-alt.info>
+ * Released under the ISC License
+ * See provided license.txt file.
+ *
+ * Date: 09/29/2014
+ */
+
+// Revealing module pattern with loose augmentation
+var lean = (function(window, module) {
+  'use strict';
+
+  var rSimpleQuery;
+
+  // simply identify ID, ELEMENT oder CLASS queries
+  rSimpleQuery = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/;
+
+  // The CSS query function
+  function cq(selectorString, contextElement) {
+
+    var selector = selectorString.trim();
+    var context = contextElement || window.document;
+
+    var simpleQuery;
+    if ((simpleQuery = rSimpleQuery.exec(selector)) !== null) {
+
+      if (typeof (simpleQuery[1]) !== 'undefined') {
+        return context.getElementById(simpleQuery[1]);
+      }
+      if (typeof (simpleQuery[2]) !== 'undefined') {
+        return context.getElementsByTagName(simpleQuery[2]);
+      }
+      return context.getElementsByClassName(simpleQuery[3]);
+    }
+
+    return context.querySelectorAll(selector);
+  }
+
+  module.cq = cq;
+  return module;
+
+}(window, lean || {}));
