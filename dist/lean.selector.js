a54490c1ba5b372d8df1bd36a2b4285a6f12d4c4
Michael Alt <lean@michael-alt.info>
2014-10-09 21:25:16 +0200

    Refactoring: Universal Module Definition

--- dist/lean.selector.js
+++ dist/lean.selector.js
@@ -5,11 +5,29 @@
  * Released under the ISC License
  * See provided license.txt file.
  *
- * Date: 09/29/2014
+ * Date: 10/09/2014
  */
 
-// Revealing module pattern with loose augmentation
-var lean = (function(window, module) {
+// Universal Module Definition (https://github.com/umdjs/umd/blob/master/returnExports.js)
+(function (root, factory) {
+  'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    if( typeof root.lean === 'undefined') {
+      root.lean = {};
+    }
+    root.lean.cq = factory();
+  }
+}(this, function () {
   'use strict';
 
   var rSimpleQuery;
@@ -18,10 +36,10 @@ var lean = (function(window, module) {
   rSimpleQuery = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/;
 
   // The CSS query function
-  function cq(selectorString, contextElement) {
+  function cssQuery(selectorString, contextElement) {
 
     var selector = selectorString.trim();
-    var context = contextElement || window.document;
+    var context = contextElement || document;
 
     var simpleQuery;
     if ((simpleQuery = rSimpleQuery.exec(selector)) !== null) {
@@ -38,7 +56,6 @@ var lean = (function(window, module) {
     return context.querySelectorAll(selector);
   }
 
-  module.cq = cq;
-  return module;
+  return cssQuery;
 
-}(window, lean || {}));
+}));
